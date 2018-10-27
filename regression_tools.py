c3cfe638db26abfd636370a7d753fd5e82b1fe53
morgoth95 <diego.fiori@epfl.ch>
2018-10-27 12:42:17 +0200

    create_submission added

--- regression_tools.py
+++ regression_tools.py
@@ -296,6 +296,19 @@ def logistic_regression_gradient_descent_demo(y, tx, gamma, max_iter, threshold)
             break
     return loss,w
 
+def create_csv_submission(ids, y_pred, name):
+    """
+    Creates an output file in csv format for submission to kaggle
+    Arguments: ids (event ids associated with each prediction)
+               y_pred (predicted class labels)
+               name (string name of .csv output file to be created)
+    """
+    with open(name, 'w') as csvfile:
+        fieldnames = ['Id', 'Prediction']
+        writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
+        writer.writeheader()
+        for r1, r2 in zip(ids, y_pred):
+            writer.writerow({'Id':int(r1),'Prediction':int(r2)})
               
 def AIC(w,l):
     """
