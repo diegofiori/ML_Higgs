64b06f481f12561e44f1a995aca2992bfd57a814
valerio-volpe <43566822+valerio-volpe@users.noreply.github.com>
2018-10-29 17:31:37 +0100

    VALERIO

--- implementations.py
+++ implementations.py
@@ -175,7 +175,7 @@ def logistic_regression_newton_method_demo(y, tx, max_iter, threshold, gamma):
     # start the logistic regression
     for iter in range(max_iter):
         # get loss and update w.
-        loss, w = learning_by_newton_method(y.reshape(-1,1), tx, w, gamma)
+        w, loss = learning_by_newton_method(y.reshape(-1,1), tx, w, gamma)
         # converge criterion
         losses.append(loss)
         if len(losses) > 1 and np.abs(losses[-1] - losses[-2])/np.abs(losses[-1]) < threshold:
