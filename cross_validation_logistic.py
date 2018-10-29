a23a41bce2f16fb513432d42f8b5fcdee4c8448a
valerio-volpe <43566822+valerio-volpe@users.noreply.github.com>
2018-10-29 20:54:35 +0100

    VALE

--- cross_validation_logistic.py
+++ cross_validation_logistic.py
@@ -3,7 +3,7 @@ from regression_tools import *
 from preprocessing import *
 from implementations import *
 
-def build_k_indices(y, k_fold, seed): ########USATO NON TOCCARE
+def build_k_indices(y, k_fold, seed):
     """build k indices for k-fold."""
     num_row = y.shape[0]
     interval = int(num_row / k_fold)
@@ -15,7 +15,7 @@ def build_k_indices(y, k_fold, seed): ########USATO NON TOCCARE
 
 def logistic_cross_validation(y, phi, k_indices, k, param_, degree, nmc , interactions, logistic_type, max_iter, threshold):
     """
-    Return the loss of logistic regression.   ########USATO NON TOCCARE
+    Return the loss of logistic regression.
     """
     
     # Create the test and train samples
@@ -31,8 +31,6 @@ def logistic_cross_validation(y, phi, k_indices, k, param_, degree, nmc , intera
     if logistic_type==0:
         # In this case the parameter is GAMMA
         w, loss = logistic_regression(y_train, x_train, initial_w, max_iter, param_)
-    #elif logistic_type==1: #mettere sgd SE LO METTIAMO RISCRIVERE IL COMMENTO
-    #    w, loss = logistic_regression_newton_method_demo(y_train, x_train, max_iter, threshold, gamma)
     elif logistic_type==2:
         # In this case the parameter is LAMBDA
         initial_w = 5 * np.ones(x_train.shape[1])
@@ -48,7 +46,7 @@ def cross_validation_logistic_demo(y_train_input,x_train,degrees,k_fold,paramete
     """
     Performs cross validation.
     logistic_type:
-        0: gradient descent                       ########USATO NON TOCCARE
+        0: gradient descent                     
         2: penalised gradient descent
     """
     
